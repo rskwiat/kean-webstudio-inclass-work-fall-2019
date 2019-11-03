3218803b473d72c586aa4a6e8f2d72946328f838
Robert Skwiat <rskwiat@gmail.com>
2019-11-03 15:48:09 -0500

    week 10 starter files

---
+++ week-10-starter/assets/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const openMenu = document.getElementById('openMenu');
+  const closeMenu = document.getElementById('closeMenu');
+  const overlay = document.getElementById('overlay');
+  const navSidebar = document.getElementById('navSidebar');
+
+  const bp = {
+    'sm': 767,
+    'lg': 1200
+  };
+
+});
